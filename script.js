451242aab327f25b698ce4c45258982c3b3a50a2
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-03-23 17:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+class OutOfRangeError extends Error {
+  constructor(arg) {
+    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+    this.name = this.constructor.name;
+  }
+}
+
+class InvalidExprError extends Error {
+  constructor() {
+    super("Expression should not have an invalid combination of expression");
+    this.name = this.constructor.name;
+  }
+}
+
+function evalString(str) {
+  try {
+    if (/[\+\-\*\/]{2,}/.test(str)) {
+      throw new InvalidExprError();
+    }
+    if (/^[\+\*\/]/.test(str)) {
+      throw new SyntaxError("Expression should not start with invalid operator");
+    }
+    if (/[\+\*\/\-]$/.test(str)) {
+      throw new SyntaxError("Expression should not end with invalid operator");
+    }
+    const result = eval(str);
+    if (!Number.isInteger(result)) {
+      throw new OutOfRangeError("non-integer result");
+    }
+    return result;
+  } catch (e) {
+    if (e instanceof OutOfRangeError || e instanceof InvalidExprError || e instanceof SyntaxError) {
+      throw e;
+    }
+    throw new OutOfRangeError(e.message);
+  }
+}
